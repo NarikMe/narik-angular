01fc7fe548811969548950b924b572165cc04f32
ali <abadakhshan@gmail.com>
2019-05-27 18:32:01 +0430

    fix toolbar position on RTL

--- projects/narik-ui-primeng/src/lib/narik-prime-toolbar/narik-prime-toolbar.component.ts
+++ projects/narik-ui-primeng/src/lib/narik-prime-toolbar/narik-prime-toolbar.component.ts
@@ -1,6 +1,13 @@
 import { UUID } from "angular2-uuid";
 import { NarikToolBar } from "narik-ui-core";
-import { Component, Injector, ViewContainerRef, OnInit } from "@angular/core";
+import {
+  Component,
+  Injector,
+  ViewContainerRef,
+  OnInit,
+  Inject
+} from "@angular/core";
+import { DOCUMENT } from "@angular/common";
 
 @Component({
   selector: "narik-toolbar , narik-prime-toolbar",
@@ -8,12 +15,15 @@ import { Component, Injector, ViewContainerRef, OnInit } from "@angular/core";
 })
 export class NarikPrimeToolBar extends NarikToolBar implements OnInit {
   uniqueId = "";
+  isRtl = false;
   toolbarItems: any[];
   constructor(
     injector: Injector,
-    viewContainerRef: ViewContainerRef
+    viewContainerRef: ViewContainerRef,
+    @Inject(DOCUMENT) private document
   ) {
     super(injector, viewContainerRef);
     this.uniqueId = "toolbar" + UUID.UUID();
+    this.isRtl = this.document.dir === "rtl";
   }
 }
