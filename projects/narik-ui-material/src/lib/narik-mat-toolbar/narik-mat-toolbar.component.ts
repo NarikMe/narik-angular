3b4db1dce5e958830251d2c13332ae3c4b803c1c
ali badakhshan <abadakhshan@gmail.com>
2019-06-13 11:01:10 +0430

    fix narik-toolbar selector problem

--- projects/narik-ui-material/src/lib/narik-mat-toolbar/narik-mat-toolbar.component.ts
+++ projects/narik-ui-material/src/lib/narik-mat-toolbar/narik-mat-toolbar.component.ts
@@ -2,7 +2,7 @@ import { NarikToolBar } from "narik-ui-core";
 import { Component, Injector, ViewContainerRef } from "@angular/core";
 
 @Component({
-  selector: "narik-toolbar narik-mat-toolbar",
+  selector: "narik-toolbar , narik-mat-toolbar",
   templateUrl: "narik-mat-toolbar.component.html"
 })
 export class NarikMatToolBar extends NarikToolBar {
