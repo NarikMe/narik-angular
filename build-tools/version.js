80f1f1ca6d95c342ffa836a4909d6fcde6bd48f4
ali <abadakhshan@gmail.com>
2021-01-20 19:42:29 +0330

    upgrade to 5.2.0

--- build-tools/version.js
+++ build-tools/version.js
@@ -1,22 +1,25 @@
-const fs = require("fs");
-const version = "5.1.0";
-const { lstatSync, readdirSync } = require("fs");
-const { join } = require("path");
+const fs = require('fs');
+const version = '5.2.0';
+const { lstatSync, readdirSync } = require('fs');
+const { join } = require('path');
 
-const isDirectory = source => lstatSync(source).isDirectory();
-const getDirectories = source =>
-  readdirSync(source)
-    .map(name => join(source, name))
-    .filter(isDirectory);
+const isDirectory = (source) => lstatSync(source).isDirectory();
+const getDirectories = (source) =>
+    readdirSync(source)
+        .map((name) => join(source, name))
+        .filter(isDirectory);
 
-const dirs = getDirectories("./projects/");
+const dirs = getDirectories('./projects/');
 console.log(version);
 
 for (var dir of dirs) {
-  const packageJson = JSON.parse(
-    fs.readFileSync(dir + "/package.json").toString()
-  );
-  console.log(dir + ":::" + packageJson.version);
-  packageJson.version = version;
-  fs.writeFileSync(dir + "/package.json", JSON.stringify(packageJson, null, 2));
+    const packageJson = JSON.parse(
+        fs.readFileSync(dir + '/package.json').toString()
+    );
+    console.log(dir + ':::' + packageJson.version);
+    packageJson.version = version;
+    fs.writeFileSync(
+        dir + '/package.json',
+        JSON.stringify(packageJson, null, 2)
+    );
 }
