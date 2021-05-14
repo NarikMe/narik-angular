f188eb2c958af24d03a4ebb982f9464e54e3a2fa
Ali Badakhshan <abadakhshan@gmail.com>
2021-05-14 12:07:26 +0430

    Support webcomponents (#18)
    * add support for display html tags & webcomponents
    
    * upgrade dependencies
    
    * fix primeng bug
    
    * fix tslib version
    
    * support web components

--- projects/narik-common/src/lib/directives/component-loader.directive.ts
+++ projects/narik-common/src/lib/directives/component-loader.directive.ts
@@ -11,8 +11,10 @@ import {
     StaticProvider,
     ComponentFactory,
     NgModuleRef,
+    RendererFactory2,
 } from '@angular/core';
 import { takeWhile } from 'rxjs/operators';
+import { isString } from '../utils/string.util';
 
 /**
  * NarikComponentLoaderDirective
@@ -28,7 +30,7 @@ export class NarikComponentLoaderDirective implements OnInit, OnDestroy {
      * Type of component.
      */
     @Input('narikComponentLoader')
-    component: Type<any> | ComponentFactory<any>;
+    component: Type<any> | ComponentFactory<any> | string;
 
     // tslint:disable-next-line:no-input-rename
 
@@ -64,60 +66,104 @@ export class NarikComponentLoaderDirective implements OnInit, OnDestroy {
     constructor(
         private componentFactoryResolver: ComponentFactoryResolver,
         private viewContainerRef: ViewContainerRef,
-        private injector: Injector
+        private injector: Injector,
+        private rendererFactory: RendererFactory2
     ) {}
 
     ngOnInit() {
-        const componentFactory =
-            this.component instanceof ComponentFactory
-                ? this.component
-                : this.componentFactoryResolver.resolveComponentFactory(
-                      this.component
-                  );
         this.viewContainerRef.clear();
+        if (isString(this.component)) {
+            const renderer = this.rendererFactory.createRenderer(null, null);
+            const element = renderer.createElement(this.component);
 
-        let parentInjector = this.injector;
-        if ((componentFactory as any).ngModule) {
-            parentInjector = ((componentFactory as any)
-                .ngModule as NgModuleRef<any>).injector;
-        }
+            renderer.insertBefore(
+                renderer.parentNode(
+                    this.viewContainerRef.element.nativeElement
+                ),
+                element,
+                this.viewContainerRef.element.nativeElement
+            );
 
-        const localInjector = Injector.create({
-            parent: parentInjector,
-            providers: this.providers || [],
-        });
-
-        const componentRef = this.viewContainerRef.createComponent(
-            componentFactory,
-            undefined,
-            localInjector
-        );
-        if (this.parameters) {
-            for (const key of Object.keys(this.parameters)) {
-                (<any>componentRef.instance)[key] = this.parameters[key];
-            }
-        }
-        if (this.bindings) {
-            for (const key of Object.keys(this.bindings)) {
-                (<any>componentRef.instance)[key] = this.bindings[key];
-                if (
-                    this.bindingSource &&
-                    <EventEmitter<any>>this.bindingSource[key + 'Change']
-                ) {
-                    (<EventEmitter<any>>this.bindingSource[key + 'Change'])
-                        .pipe(takeWhile((x) => this.isAlive))
-                        .subscribe((result) => {
-                            (<any>componentRef.instance)[key] = result;
-                        });
+            const data = this.parameters;
+            if (data) {
+                for (const key in data) {
+                    if (Object.prototype.hasOwnProperty.call(data, key)) {
+                        element[key] = data[key];
+                    }
                 }
             }
-        }
-        if (this.events) {
-            for (const key of Object.keys(this.events)) {
-                if (componentRef.instance[key]) {
-                    (<EventEmitter<any>>componentRef.instance[key]).subscribe(
-                        this.events[key]
-                    );
+            if (this.events) {
+                for (const key of Object.keys(this.events)) {
+                    element.addEventListener(key, this.events[key]);
+                }
+            }
+
+            if (this.bindings) {
+                for (const key of Object.keys(this.bindings)) {
+                    element[key] = this.bindings[key];
+                    if (
+                        this.bindingSource &&
+                        <EventEmitter<any>>this.bindingSource[key + 'Change']
+                    ) {
+                        (<EventEmitter<any>>this.bindingSource[key + 'Change'])
+                            .pipe(takeWhile((x) => this.isAlive))
+                            .subscribe((result) => {
+                                element[key][key] = result;
+                            });
+                    }
+                }
+            }
+        } else {
+            const componentFactory =
+                this.component instanceof ComponentFactory
+                    ? this.component
+                    : this.componentFactoryResolver.resolveComponentFactory(
+                          this.component
+                      );
+
+            let parentInjector = this.injector;
+            if ((componentFactory as any).ngModule) {
+                parentInjector = ((componentFactory as any)
+                    .ngModule as NgModuleRef<any>).injector;
+            }
+
+            const localInjector = Injector.create({
+                parent: parentInjector,
+                providers: this.providers || [],
+            });
+
+            const componentRef = this.viewContainerRef.createComponent(
+                componentFactory,
+                undefined,
+                localInjector
+            );
+            if (this.parameters) {
+                for (const key of Object.keys(this.parameters)) {
+                    (<any>componentRef.instance)[key] = this.parameters[key];
+                }
+            }
+            if (this.bindings) {
+                for (const key of Object.keys(this.bindings)) {
+                    (<any>componentRef.instance)[key] = this.bindings[key];
+                    if (
+                        this.bindingSource &&
+                        <EventEmitter<any>>this.bindingSource[key + 'Change']
+                    ) {
+                        (<EventEmitter<any>>this.bindingSource[key + 'Change'])
+                            .pipe(takeWhile((x) => this.isAlive))
+                            .subscribe((result) => {
+                                (<any>componentRef.instance)[key] = result;
+                            });
+                    }
+                }
+            }
+            if (this.events) {
+                for (const key of Object.keys(this.events)) {
+                    if (componentRef.instance[key]) {
+                        (<EventEmitter<any>>(
+                            componentRef.instance[key]
+                        )).subscribe(this.events[key]);
+                    }
                 }
             }
         }
