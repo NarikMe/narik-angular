65b3ac1fe32f0050d18c4f180f7b4e74a71475b8
ali <abadakhshan@gmail.com>
2021-06-03 22:10:37 +0430

    add @directive()

--- projects/narik-infrastructure/src/lib/base/narik.component.ts
+++ projects/narik-infrastructure/src/lib/base/narik.component.ts
@@ -1,9 +1,10 @@
-import { OnInit, OnDestroy } from '@angular/core';
+import { OnInit, OnDestroy, Directive } from '@angular/core';
 
 /**
  * Narik component
  * Base class for all components
  */
+@Directive()
 export abstract class NarikComponent implements OnInit, OnDestroy {
     /**
      * Determines whether component is  alive or not
