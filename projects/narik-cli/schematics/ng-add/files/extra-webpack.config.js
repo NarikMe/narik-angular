3500ea71d9727c3d7554c3735cfcb40ff1bc2e05
ali <abadakhshan@gmail.com>
2022-01-13 17:02:08 +0330

    fix removing plugins

--- projects/narik-cli/schematics/ng-add/files/extra-webpack.config.js
+++ projects/narik-cli/schematics/ng-add/files/extra-webpack.config.js
@@ -3,35 +3,34 @@ const AngularCompilerPlugin = require('@ngtools/webpack/src');
 const LayoutResolver = require('./build-tools/layout-resolver');
 
 module.exports = (config) => {
-    const index = config.plugins.findIndex((p) => {
-        return p instanceof AngularCompilerPlugin.AngularWebpackPlugin;
-    });
-    const options = config.plugins[index].pluginOptions;
-    options.directTemplateLoading = false;
-    config.plugins.splice(index, 1);
-
-    config.plugins.push(
-        new AngularCompilerPlugin.AngularWebpackPlugin(options)
-    );
-
-    config.module.rules.unshift(
+  const index = config.plugins.findIndex((p) => {
+    return p instanceof AngularCompilerPlugin.AngularWebpackPlugin;
+  });
+  const options = config.plugins[index].pluginOptions;
+  options.directTemplateLoading = false;
+  config.plugins.splice(
+    index,
+    1,
+    new AngularCompilerPlugin.AngularWebpackPlugin(options)
+  );
+  config.module.rules.unshift(
+    {
+      test: /\.html$/i,
+      use: [
         {
-            test: /\.html$/i,
-            use: [
-                {
-                    loader: '@narik/webpack-tools',
-                    options: {
-                        resolver: new LayoutResolver(),
-                        basePath: path.dirname(options.tsconfig),
-                    },
-                },
-            ],
+          loader: '@narik/webpack-tools',
+          options: {
+            resolver: new LayoutResolver(),
+            basePath: path.dirname(options.tsconfig),
+          },
         },
-        {
-            test: /\.html$/i,
-            type: 'asset/source',
-        }
-    );
+      ],
+    },
+    {
+      test: /\.html$/i,
+      type: 'asset/source',
+    }
+  );
 
-    return config;
+  return config;
 };
