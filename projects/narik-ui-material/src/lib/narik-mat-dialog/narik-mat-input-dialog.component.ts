afbfd23f08c494ff96b0e37c850f8d9ee9aa41f6
ali <abadakhshan@gmail.com>
2022-05-27 09:03:44 +0430

    form support

--- projects/narik-ui-material/src/lib/narik-mat-dialog/narik-mat-input-dialog.component.ts
+++ projects/narik-ui-material/src/lib/narik-mat-dialog/narik-mat-input-dialog.component.ts
@@ -1,5 +1,15 @@
 import { Component, OnInit, Inject } from '@angular/core';
-import { DialogInputContent, PARAMETERS } from '@narik/infrastructure';
+import {
+    FormBuilder,
+    FormGroup,
+    ValidatorFn,
+    Validators,
+} from '@angular/forms';
+import {
+    DialogInputContent,
+    EntityField,
+    PARAMETERS,
+} from '@narik/infrastructure';
 
 @Component({
     templateUrl: 'narik-mat-input-dialog.component.html',
@@ -7,12 +17,43 @@ import { DialogInputContent, PARAMETERS } from '@narik/infrastructure';
 export class NarikMatInputDialog implements OnInit, DialogInputContent {
     entity: any = {};
     fields: any[] = [];
-    constructor(@Inject(PARAMETERS) parameters: any) {
+    form: FormGroup;
+    constructor(
+        @Inject(PARAMETERS) parameters: any,
+        private formBuilder: FormBuilder
+    ) {
         if (parameters) {
             this.entity = parameters.entity;
             this.fields = parameters.fields;
+
+            this.form = this.buildForm();
+
+            this.form.patchValue(this.entity);
         }
     }
 
     ngOnInit() {}
+
+    protected buildForm(): FormGroup {
+        const controls = {};
+        if (this.fields) {
+            for (const field of this.fields) {
+                controls[field.name] = [
+                    undefined,
+                    Validators.compose(this.createValidators(field)),
+                ];
+            }
+        }
+        return this.formBuilder.group(controls);
+    }
+
+    createValidators(field: EntityField): (ValidatorFn | null | undefined)[] {
+        const validators = [];
+
+        if (field.required) {
+            validators.push(Validators.required);
+        }
+
+        return validators;
+    }
 }
